6a2d137d5776c0cf50e35939140f9f64e92bea31
Müşerref Selçuk Özdemir <s_05_60@hotmail.com>
2023-01-15 16:31:42 +0300

    server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+//import { Configuration, OpenAIApi } from 'openai'
 import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
@@ -14,25 +15,67 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+
+
+import { Configuration, OpenAIApi } from 'openai'
 // import { users, posts } from "./data/index.js";
 // import User from ".models/User.js"
 // import Post from ".models/Post.js"
 
 
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
+const openai = new OpenAIApi(configuration);
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  })
+})
+
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    });
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "100mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
@@ -54,14 +97,50 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
+//------------------------------------------------------------------------------------------------
 
+// // //const configuration = new Configuration({
+// //   apiKey: process.env.OPENAI_API_KEY,
+// // });
 
+// //const openai = new OpenAIApi(configuration);
 
+// app.use(express.json())
+
+// app.get('/ai', async (req, res) => {
+//   res.status(200).send({
+//     message: 'Hello from CodeX!'
+//   })
+// })
+
+// app.post('/', async (req, res) => {
+//   try {
+//     const prompt = req.body.prompt;
+
+//     const response = await openai.createCompletion({
+//       model: "text-davinci-003",
+//       prompt: `${prompt}`,
+//       temperature: 0, // Higher values means the model will take more risks.
+//       max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+//       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+//       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+//       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+//     });
+
+//     res.status(200).send({
+//       bot: response.data.choices[0].text
+//     });
+
+//   } catch (error) {
+//     console.error(error)
+//     res.status(500).send(error || 'Something went wrong');
+//   }
+// })
 
 
 /* MONGOOSE SETUP */
 mongoose.set('strictQuery', false);
-const PORT = process.env.PORT || 6001;
+const PORT = process.env.PORT || 3001;
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -75,3 +154,4 @@ mongoose
     //  Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
+
